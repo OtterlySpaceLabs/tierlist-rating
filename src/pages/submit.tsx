d13be839700b3f288e8c7af523762d5399e222dd
Alexandre Sanchez <contact@alexandresanchez.xyz>
2023-05-27 01:02:40 +0200

    feat: favicons

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head"
 import Header from "../components/header"
 import SubmissionForm from "../components/submission/submissionForm"
 
 export default function SubmitPage() {
 	return (
 		<>
+			<Head>
+				<title>Smash App - Submit</title>
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+				<link rel="shortcut icon" href="/favicons/favicon.ico" />
+			</Head>
 			<Header />
 			<main className="flex flex-col items-center p-8">
 				<h1 className="text-center text-2xl">Submit your character</h1>
