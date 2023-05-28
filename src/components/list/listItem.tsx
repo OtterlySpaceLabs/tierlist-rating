503a7ead58b09f793f78c028ddebd5872fecd16e
Alexandre Sanchez <contact@alexandresanchez.xyz>
2023-05-28 02:43:01 +0200

    feat: improve approve/reject buttons ux

--- src/components/list/listItem.tsx
+++ src/components/list/listItem.tsx
@@ -101,7 +101,10 @@ export default function ListItem({ submission, moderationMode, showDeleteDialog,
 					<div className="mt-2 flex gap-2">
 						<button
 							type="button"
-							className="inline-flex items-center gap-x-1.5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+							className={cn(
+								"inline-flex items-center gap-x-1.5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600",
+								submission.status === "APPROVED" && "hidden"
+							)}
 							onClick={() => void setStatus("APPROVED")}
 						>
 							<CheckCircleIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
@@ -109,7 +112,10 @@ export default function ListItem({ submission, moderationMode, showDeleteDialog,
 						</button>
 						<button
 							type="button"
-							className="inline-flex items-center gap-x-1.5 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+							className={cn(
+								"inline-flex items-center gap-x-1.5 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600",
+								submission.status === "REJECTED" && "hidden"
+							)}
 							onClick={() => void setStatus("REJECTED")}
 						>
 							<XCircleIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
